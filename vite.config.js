c141a87a9b04e4811718fec907c9b4dfc15caf1f
Vini0667 <vini04.silva09@gmail.com>
2025-12-08 14:28:42 -0400

    Devops: Updated the workflows

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,20 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import istanbul from "vite-plugin-istanbul";
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [react(), tailwindcss()],
+    plugins: [
+        react(),
+        tailwindcss(),
+        istanbul({
+            cypress: true,
+            include: ["src/**/*"],
+            exclude: ["node_modules", "cypress/**"],
+            extension: [".js", ".jsx", ".ts", ".tsx"],
+        }),
+    ],
     server: {
         port: 3000,
         host: "0.0.0.0",
